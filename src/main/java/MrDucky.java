a1d33e01dc38bbe81e5e94af88edb6cbcf8902a0
janelle <lumjanelle@gmail.com>
2026-01-23 14:08:50 +0800

    Level-2: Add the ability to store whatever text entered by the user and display them back to the user when requested.

--- src/main/java/MrDucky.java
+++ src/main/java/MrDucky.java
@@ -1,29 +1,41 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class MrDucky {
     public static void main(String[] args) {
         String line = "____________________________________________________________";
-        // Read user input
         Scanner sc = new Scanner(System.in);
+        List<String> items = new ArrayList<>();
 
+        // Greeting message to the user
         System.out.println(line);
         System.out.println("Hello! I'm MrDucky");
         System.out.println("What can I do for you?");
         System.out.println(line);
 
+        // Loop: Read line, if bye then exit, else if list, print all stored items 
+        // else: add to list, print added: <input>
         while (true) {
             String input = sc.nextLine();
         
             if (input.equals("bye")){
+                System.out.println(line);
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println(line);
                 break; // Exit the loop and program
             }
-            else {
-                System.out.println("Quack! You said: " + input);
+            if (input.equals("list")) {
                 System.out.println(line);
+                for (int i = 0; i < items.size(); i++) {
+                    System.out.println((i + 1) + "." + items.get(i));
+                }
+                continue;
             } 
+            items.add(input);
+            System.out.println(line);
+            System.out.println("added: " + input);
+            System.out.println(line);
         }
-        sc.close();
     }
 }
